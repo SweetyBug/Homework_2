184d45d5810917d51f3c71b5bd8b36cd63050028
Терехин Максим Александрович <MTerekhin@synergy.ru>
2022-03-02 17:46:20 +0300

    Finish code

---
+++ Задача_13/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberFirst = Convert.ToInt32(Console.ReadLine());
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+int ost = numberFirst % numberSecond;
+if (ost == 0)
+{
+    Console.WriteLine("Остаток: Кратно");
+}
+else
+{
+    Console.WriteLine("Остаток: " + ost);
+}
